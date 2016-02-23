0cdd8bf317f95f0b03e86476c421ee2c6db2ab1f
Kim Helberg <kim.helberg@guardsystems.com>
2016-02-23 20:29:20 +0000

    Removed some debugging echos.

--- npm_require.php
+++ npm_require.php
@@ -1,16 +1,11 @@
 <?php
 function npm_require($modulename) {
-	echo 'Require: '.$modulename.PHP_EOL;
 	return new npm_module($modulename);
 }
 
 class npm_module {
 	private $props = array();
 	function __construct($modulename) {
-		echo 'Constructing: '.$modulename.PHP_EOL;
-		
-		echo 'Executing: '.'nodejs -e "var m=require(\"'.$modulename.'\"); console.log(m);"'.PHP_EOL;
-
 		$mod = shell_exec('nodejs -e "var m=require(\"'.$modulename.'\");'.
 			'if(typeof m===\"function\"){console.log(\"self function\");}'.
 			'Object.keys(m).forEach(function(k){console.log(k, m[k]);})"'.PHP_EOL);
@@ -23,7 +18,6 @@ class npm_module {
 				$this->props['self'] = function() use ($modulename, $name) {
 					$args = json_encode(func_get_args());
 					$args = substr($args, 1, strlen($args) - 2);
-					echo 'Called clojure m('.$args.')'.PHP_EOL;
 					
 					$ret = json_decode(trim(shell_exec('nodejs -e "var m=require(\"'.$modulename.'\");'.
 						'console.log(JSON.stringify(m('.addslashes($args).')));"'.PHP_EOL)));
@@ -34,7 +28,6 @@ class npm_module {
 				$this->props[$name] = function() use ($modulename, $name) {
 					$args = json_encode(func_get_args());
 					$args = substr($args, 1, strlen($args) - 2);
-					echo 'Called clojure m[\''.$name.'\']('.$args.')'.PHP_EOL;
 					$ret = json_decode(trim(shell_exec('nodejs -e "var m=require(\"'.$modulename.'\");'.
 						'console.log(m[\''.$name.'\']('.addslashes($args).'));"'.PHP_EOL)));
 
@@ -50,19 +43,16 @@ class npm_module {
 	}
 	
 	function __invoke() {
-		echo 'Invoked object as function.'.PHP_EOL;
 		if(isset($this->props['self'])) {
 			return call_user_func_array($this->props['self'], func_get_args());
 		}
 	}
 	function __call($name, $arguments) {
-		echo 'Called method '.$name.PHP_EOL;
 		if(isset($this->props[$name])) {
 			return call_user_func_array($this->props[$name], $arguments);
 		}
 	}
 	function __get($name) {
-		echo 'Get property '.$name.PHP_EOL;
 		if(isset($this->props[$name])) {
 			return $this->props[$name];
 		}
--- test.php
+++ test.php
@@ -9,7 +9,7 @@ $cars = array(
 	array('model'=> 'tesla', 'make'=> 's', 'km'=> 120)
 );
 
-$aggsy('model(_sum(km),_count())', $cars);
+print_r($aggsy('model(_sum(km),_count())', $cars));
 
 echo 'Test: '.$aggsy->test(4, 5).PHP_EOL.$aggsy->foo.PHP_EOL.$aggsy->bar.PHP_EOL;
 ?>
